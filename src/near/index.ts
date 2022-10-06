raendev/api

commit bba9a3aff091e180f07b528ebc5e61a64733f653

fix: switch to netlify/functions, no edge-functions

src/near/index.ts
@@ -1,4 +1,5 @@
-import * as naj from "near-api-js"
+import type { Near } from "near-api-js"
+import { InMemoryKeyStore } from "near-api-js/lib/key_stores"
 
 export * from './schema'
 
@@ -29,7 +30,7 @@ export class UnknownNetworkError extends Error {
 export interface ContractInterface {
   contract: string,
   config: typeof testnetConfig | typeof mainnetConfig
-  near: naj.Near
+  near: Near
 }
 
 /**
@@ -52,7 +53,7 @@ export function init(contract: string): ContractInterface {
 
   const near = new naj.Near({
     ...config,
-    keyStore: new naj.keyStores.InMemoryKeyStore()
+    keyStore: new InMemoryKeyStore()
   })
 
   return {

astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });

src/near/schema.ts
@@ -1,8 +1,7 @@
 import { init } from "."
 import { readCustomSection } from "wasm-walrus-tools"
 import brotliDecompress from 'brotli/decompress';
-import type { Near } from "near-api-js"
-import type { ContractCodeView } from "near-api-js/lib/providers/provider"
+import type { ContractCodeView, Near } from "near-api-js"
 import type { JSONSchema7 } from "json-schema"
 
 export async function fetchSchema(contract: string): Promise<JSONSchema7> {

commit c4aac6ea2773878353d9ab450acdebcf84bf558e

fix: naj import errors introduced in bba9a3aff091e180f07b528ebc5e61a64733f653
These were introduced during a temporary attempt to get NAJ to work with
Netlify Edge Functions

src/near/index.ts
@@ -1,5 +1,4 @@
-import type { Near } from "near-api-js"
-import { InMemoryKeyStore } from "near-api-js/lib/key_stores"
+import { keyStores, Near } from "near-api-js"
 
 export * from './schema'
 
@@ -51,9 +50,9 @@ export function init(contract: string): ContractInterface {
 
   if (!config) throw new UnknownNetworkError(contract)
 
-  const near = new naj.Near({
+  const near = new Near({
     ...config,
-    keyStore: new InMemoryKeyStore()
+    keyStore: new keyStores.InMemoryKeyStore()
   })
 
   return {