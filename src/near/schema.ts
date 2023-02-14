bcfa323ca17ea178957a60fc293f076adbe0f536
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-02-14 10:09:12 -0500

    Revert "feat: first attempt to cache contract code"
    This reverts commit c8f0776cbdd07695e04910cb188ecfb4b4a09331.

--- src/near/schema.ts
+++ src/near/schema.ts
@@ -1,10 +1,8 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
 import { init } from "."
 import { readCustomSection } from "wasm-walrus-tools"
 import brotliDecompress from 'brotli/decompress';
 import type { Near } from "near-api-js"
-import type { AccountView, ContractCodeView } from "near-api-js/lib/providers/provider"
+import type { ContractCodeView } from "near-api-js/lib/providers/provider"
 import type { JSONSchema7 } from "json-schema"
 import { DecompressionFailure, NoCustomSection } from "./errors"
 
@@ -30,69 +28,13 @@ export async function fetchSchema(contract: string): Promise<JSONSchema7> {
   return schema
 }
 
-interface CachedCode {
-  code_hash: string
-  code_base64: string
-}
-
-/**
- * Fetch base64-encoded code for given `contract` from the cache, if it's there,
- * otherwise fetch it from RPC and write it to cache.
- * 
- * If cached version found, it will be returned immediately, and a background
- * process will check if it's fresh and update it if necessary.
- */
-async function getContractCode(contract: string, near: Near): Promise<string> {
-  const cachePath = path.resolve(`src/near/cache/${contract}.json`)
-  let cached: CachedCode
-  try {
-    cached = JSON.parse(
-      await fs.readFile(cachePath, 'utf-8')
-    )
-    return cached.code_base64
-  } catch {
-    const [{ code_hash }, { code_base64 }] = await Promise.all([
-      near.connection.provider.query({
-        account_id: contract,
-        finality: 'final',
-        request_type: 'view_account',
-      }),
-      near.connection.provider.query({
-        account_id: contract,
-        finality: 'final',
-        request_type: 'view_code',
-      })
-    ]) as [AccountView, ContractCodeView]
-    await fs.writeFile(
-      cachePath,
-      JSON.stringify({ code_hash, code_base64 }),
-      'utf-8'
-    )
-    return code_base64
-  }
-  // TODO: move to separate cloud function, which we call in a fire-and-forget way
-  // const { code_hash } = await near.connection.provider.query({
-  //   account_id: contract,
-  //   finality: 'final',
-  //   request_type: 'view_account',
-  // }) as AccountView
-  // if (cached.code_hash !== code_hash) {
-  //   const { code_base64 } = await near.connection.provider.query({
-  //     account_id: contract,
-  //     finality: 'final',
-  //     request_type: 'view_code',
-  //   }) as ContractCodeView
-  //   await fs.writeFile(
-  //     cachePath,
-  //     JSON.stringify({ code_hash, code_base64 }),
-  //     'utf-8'
-  //   )
-  // }
-}
-
 async function fetchJsonAddressOrData(contract: string, near: Near): Promise<string> {
-  const code_base64 = await getContractCode(contract, near)
-  const wasmStr = atob(code_base64)
+  const code = await near.connection.provider.query({
+    account_id: contract,
+    finality: 'final',
+    request_type: 'view_code',
+  }) as ContractCodeView
+  const wasmStr = atob(code.code_base64)
   const wasm = new Uint8Array(new ArrayBuffer(wasmStr.length));
   for (let i = 0; i < wasmStr.length; i++) { wasm[i] = wasmStr.charCodeAt(i) }
   const jsonCustomSection = await readCustomSection(wasm, "json")
